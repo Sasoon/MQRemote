d712114831cadf4ba5c29b181a96c9e70d146242
unknown <Administrator@FBE-3006996.mqauth.uni.mq.edu.au>
2019-03-11 11:16:10 +1100

    Some style changes

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
-import { NativeModules, TouchableOpacity, Image, StatusBar, StyleSheet, Text, View } from "react-native";
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+  NativeModules,
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
+import { Button, Header } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 import Room from "./components/Room";
 import Toggle from "./components/Toggle";
 
 const STATUS_BAR_HEIGHT = StatusBar.currentHeight;
-const bg = "https://staff.mq.edu.au/media/photos/shared_identity_three-colour_gradient.jpg";
-const icon = "https://webresources.mq.edu.au/mq_templates/global/images/2015/logo.png";
+const bg =
+  "https://staff.mq.edu.au/media/photos/shared_identity_three-colour_gradient.jpg";
+const icon =
+  "https://webresources.mq.edu.au/mq_templates/global/images/2015/logo.png";
+
 
 export default class App extends Component {
   constructor(props) {
@@ -41,43 +51,34 @@ export default class App extends Component {
   };
   render() {
     return (
-
       <View style={styles.container}>
+      <Header backgroundColor="#A41F31"
+  centerComponent={{ text: 'MQRemote', style: { color: '#fff', fontSize: 30, fontWeight: 'bold', paddingBottom: 25 } }}
+/>
+        <View style={styles.header}>
+          <Image
+            source={{ uri: icon }}
+            style={{ height: 60, width: 120 }}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.container}>
+          <Room roomNumber="104" />
+          <Room roomNumber="118" />
+          <Room roomNumber="206" />
+          <Room roomNumber="208" />
+          <Room roomNumber="214" />
+          <Room roomNumber="316" />
+        </View>
 
-                            <Image
-              source={{ uri: bg }}
-              style={{ position: "absolute", height: "100%", width: "100%" }}
-              resizeMode="cover"
-            />
-          <View style={styles.header}>
-                            <Image
-              source={{ uri: icon }}
-              style={{ height: 60, width: 120 }}
-              resizeMode="contain"
-            />
-            <Text style={{ fontSize: 30, color: "#ffff", fontWeight: "bold", paddingLeft: 15, paddingRight: 15 }}>
-              MQRemote
-            </Text>
-          </View>
-          <View>
-            <Room roomNumber="104"></Room>
-            <Room roomNumber="118"></Room>
-            <Room roomNumber="206"></Room>
-            <Room roomNumber="208"></Room>
-            <Room roomNumber="214"></Room>
-            <Room roomNumber="316"></Room>
-          </View>
-          
-
-              <Text> Wol is {this.state.isOn ? "ON" : "OFF"} </Text>
-    <Button onPress={this.Start} title="Start" />
-    {!this.state.isOn ? (
-      <Button onPress={this.turnOn} title="Turn ON" />
-    ) : (
-      <Button onPress={this.turnOff} title="Turn OFF"/>
-    )}
-
-      </View>      
+        <Text> Wol is {this.state.isOn ? "ON" : "OFF"} </Text>
+        <Button onPress={this.Start} title="Start" />
+        {!this.state.isOn ? (
+          <Button onPress={this.turnOn} title="Turn ON" />
+        ) : (
+          <Button onPress={this.turnOff} title="Turn OFF" />
+        )}
+      </View>
     );
   }
 }
@@ -87,12 +88,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#FCFCFC"
   },
   header: {
     marginTop: STATUS_BAR_HEIGHT,
     flex: 1,
     paddingLeft: 15,
-    flexDirection: 'row'
-  },
+    flexDirection: "row"
+  }
 });
--- components/Room.js
+++ components/Room.js
@@ -2,12 +2,24 @@ import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { Button } from 'react-native-elements';
 
+// Button styles
+const RoomButton = props => <Button color='red' buttonStyle={styles.roomButton} {...props} />;
+
 export class Room extends React.Component {
   render() {
     return (
-      <Button roomNumber=""><Text>{this.props.roomNumber}</Text></Button>
-    )
+      <RoomButton roomNumber="" title={this.props.roomNumber} />    )
   }
 }
+
+const styles = StyleSheet.create({
+  roomButton: {
+    width: 100,
+    backgroundColor: 'red',
+    color: 'green',
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
 
 export default Room;
